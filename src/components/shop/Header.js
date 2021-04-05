3760daea94b80942ddcd369c27b7177d3cd34d16
Iliyan-Y <ili.yankov91@gmail.com>
2021-04-05 19:08:20 +0100

    Typo fixed

--- src/components/shop/Header.js
+++ src/components/shop/Header.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 const Header = ({ basketState }) => {
   return (
     <div id="header">
-      <a href="#">Log Our</a>
+      <Link to="/">Log Out</Link>
       <p style={{ margin: '0.1em 1em' }}> | </p>
       <Link to="/basket">Cart ({basketState.length})</Link>
     </div>
